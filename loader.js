ab2dcc9ea17cc286d49fb0d514901b9ec72936f8
Business <albara-a-s2@hotmail.com>
2026-08-08 00:05:44 +0300

    Add loader functions to show and hide loader

---
+++ loader.js
@@ -0,0 +1,20 @@
+function showLoader(){
+
+let loader=document.getElementById("loader");
+
+if(loader){
+loader.style.display="flex";
+}
+
+}
+
+
+function hideLoader(){
+
+let loader=document.getElementById("loader");
+
+if(loader){
+loader.style.display="none";
+}
+
+}
